27f6f5c283c6a5359575221cfcc0d2b8cb7d49ae
PeppermintSnow <peppermintsnow06@gmail.com>
2025-10-19 09:33:57 +0800

    chore(nvim-lsp): make lsp config coherent and list sources

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,6 +5,7 @@ return {
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       -- Servers config
+      -- Installed from pacman: lua-language-server
       vim.lsp.config["lua_ls"] = {
         cmd = { "lua-language-server" },
         filetypes = { "lua" },
@@ -17,19 +18,25 @@ return {
           },
         },
       }
+      -- Installed from pacman: clang
       vim.lsp.config["c_ls"] = {
         cmd = { "clangd" },
         filetypes = { "c", "h" },
         capabilities = capabilities,
       }
+      -- Installed from AUR/npm: vtsls
       vim.lsp.config["ts_ls"] = {
         cmd = { "vtsls", "--stdio" },
+      }
+      -- Installed from pacman: jdtls
+      vim.lsp.config["java_ls"] = {
+        cmd = { "jdtls" }
       }
       -- Enable servers
       vim.lsp.enable("lua_ls")
       vim.lsp.enable("c_ls")
       vim.lsp.enable("ts_ls")
-      vim.lsp.enable("jdtls")
+      vim.lsp.enable("java_ls")
     end
   },
 }
