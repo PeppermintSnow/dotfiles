47b9940d974bd48c43ef031f66b813943a46c213
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2025-10-30 09:46:08 +0800

    fix(nvim-lsp): add filetypes, root_markers, and capabilities on each ls

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -17,28 +17,43 @@ return {
             },
           },
         },
+        capabilities = capabilities
       }
       -- Installed from pacman: clang
       vim.lsp.config["c_ls"] = {
         cmd = { "clangd" },
         filetypes = { "c", "h" },
-        capabilities = capabilities,
+        root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
+        capabilities = capabilities
       }
       -- Installed from AUR/npm: vtsls
       vim.lsp.config["ts_ls"] = {
         cmd = { "vtsls", "--stdio" },
+        filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
+        root_markers = { { "package-lock.json", "yarn.lock", "pnpm-lock.yaml" }, ".git" },
+        capabilities = capabilities
       }
       -- Installed from pacman: jdtls
       vim.lsp.config["java_ls"] = {
-        cmd = { "jdtls" }
+        cmd = { "jdtls" },
+        filetypes = { "java" },
+        root_markers = { { "mvnw", "gradlew", "settings.gradle", "settings.gradle.kts", ".git" }, { "build.xml", "pom.xml", "build.gradle", "build.gradle.kts" } },
+        capabilities = capabilities
+
       }
       -- Installed from pacman: bash-language-server
       vim.lsp.config["bash_ls"] = {
-        cmd = { "bash-language-server", "start" }
+        cmd = { "bash-language-server", "start" },
+        filetypes = { "bash", "sh" },
+        root_markers = { ".git" },
+        capabilities = capabilities
       }
       -- Installed from pacman: python-lsp-server
       vim.lsp.config["python_ls"] = {
-        cmd = { "pylsp" }
+        cmd = { "pylsp" },
+        filetypes = { "python" },
+        root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
+        capabilities = capabilities
       }
 
       -- Enable servers
