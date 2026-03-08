095495cb64bfc6c9a984f9d610437e0a30b226cb
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2026-03-08 10:46:27 +0800

    feat(nvim-lsp): add rust ls and fix laravel cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -4,105 +4,61 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      -- Servers config
+      -- Global
+      vim.lsp.config('*', {
+        capabilities = capabilities,
+      })
+
+      -- Servers 
       -- Installed from pacman: lua-language-server
-      vim.lsp.config["lua_ls"] = {
-        cmd = { "lua-language-server" },
-        filetypes = { "lua" },
-        rootmarkers = { { ".luarc.json", ".luarc.jsonc" }, ".git" },
+      vim.lsp.config("lua_ls", {
         settings = {
           Lua = {
-            runtime = {
-              version = "LuaJIT",
-            },
+            runtime = { version = "LuaJIT" },
           },
         },
-        capabilities = capabilities
-      }
+      })
       -- Installed from pacman: clang
-      vim.lsp.config["c_ls"] = {
-        cmd = { "clangd" },
-        filetypes = { "c", "h" },
-        root_markers = { ".clangd", ".clang-tidy", ".clang-format", "compile_commands.json", "compile_flags.txt", "configure.ac", ".git" },
-        capabilities = capabilities
-      }
-      -- Installed from AUR/npm: vtsls
-      vim.lsp.config["ts_ls"] = {
-        cmd = { "vtsls", "--stdio" },
-        filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact" },
-        root_markers = { { "package-lock.json", "yarn.lock", "pnpm-lock.yaml" }, ".git" },
-        capabilities = capabilities
-      }
-      -- Installed from pacman: jdtls
-      vim.lsp.config["java_ls"] = {
-        cmd = { "jdtls" },
-        filetypes = { "java" },
-        root_markers = { { "mvnw", "gradlew", "settings.gradle", "settings.gradle.kts", ".git" }, { "build.xml", "pom.xml", "build.gradle", "build.gradle.kts" } },
-        capabilities = capabilities
-
-      }
-      -- Installed from pacman: bash-language-server
-      vim.lsp.config["bash_ls"] = {
-        cmd = { "bash-language-server", "start" },
-        filetypes = { "bash", "sh" },
-        root_markers = { ".git" },
-        capabilities = capabilities
-      }
-      -- Installed from pacman: python-lsp-server
-      vim.lsp.config["python_ls"] = {
-        cmd = { "pylsp" },
-        filetypes = { "python" },
-        root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
-        capabilities = capabilities
-      }
-      -- Installed from flutter's tarball
-      vim.lsp.config["dart_ls"] = {
-        cmd = { "dart", "language-server", "--protocol=lsp" },
-        filetypes = { "dart" },
-        root_markers = { "pubspec.yaml" },
-        init_options = {
-          closingLabels = true,
-          flutterOutline = true,
-          onlyAnalyzeProjectsWithOpenFiles = true,
-          outline = true,
-          suggestFromUnimportedLibraries = true
-        },
+      vim.lsp.config("clangd", {
+        filetypes = { "c", "h" }
+      })
+      -- Installed from pacman: rust-analyzer
+      vim.lsp.config("rust_analyzer", {
         settings = {
-          dart = {
-            completeFunctionCalls = true,
-            showTodos = true
+          ['rust-analyzer'] = {
+            diagnostics = {
+              enable = false;
+            }
           }
-        },
-        capabilities = capabilities
-      }
-
-      -- Installed from yay: phpactor
-      vim.lsp.config["php_ls"] = {
-        cmd = { "phpactor", "language-server" },
-        filetypes = { "php" },
-        root_markers = { ".git", "composer.json", ".phpactor.json", ".phpactor.yml" },
-        workspace_required = true,
-        capabilities = capabilities
-      }
-
-      -- Installed from the GitHub repository: https://github.com/laravel-ls/laravel-ls
-      vim.lsp.config["laravel_ls"] = {
-        cmd = { "laravel-ls" },
-        filetypes = { "php", "blade" },
-        rootmarkers = { "artisan" },
-        capabilities = capabilities
-      }
+        }
+      })
+      -- Installed from pacman: bash-language-server
+      vim.lsp.config("bashls", {})
+      -- Installed from AUR/npm: vtsls
+      vim.lsp.config("vtsls", {})
+      -- Installed from pacman: jdtls
+      vim.lsp.config("jdtls", {})
+      -- Installed from pacman: python-lsp-server
+      vim.lsp.config("pylsp", {})
+      -- Installed from flutter's tarball
+      vim.lsp.config("dartls", {})
+      -- Installed from npm: intelephense
+      vim.lsp.config("intelephense", {
+        root_markers = { "artisan" }
+      })
 
       -- Enable servers
-      vim.lsp.enable("lua_ls")
-      vim.lsp.enable("c_ls")
-      vim.lsp.enable("ts_ls")
-      vim.lsp.enable("java_ls")
-      vim.lsp.enable("bash_ls")
-      vim.lsp.enable("python_ls")
-      vim.lsp.enable("dart_ls")
-      vim.lsp.enable("php_ls")
-      -- vim.lsp.enable("laravel_ls")
+      vim.lsp.enable({
+        "lua_ls",
+        "clangd",
+        "rust_analyzer",
+        "bashls",
+        "vtsls",
+        "jdtls",
+        "pylsp",
+        "dartls",
+        "intelephense"
+      })
     end
   },
 }
