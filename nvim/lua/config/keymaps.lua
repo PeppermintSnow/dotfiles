3d159160694eee37b085a472b1814bea2749439f
PeppermintSnow <168705921+PeppermintSnow@users.noreply.github.com>
2026-06-12 10:06:58 +0800

    feat(nvim): remove <C-hjkl> insert mode binding

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,9 +2,3 @@
 local telescope = require("telescope.builtin")
 vim.keymap.set("n", "<leader>ff", telescope.find_files, { desc = "Telescope find files" } )
 vim.keymap.set("n", "<leader>fg", telescope.live_grep, { desc = "Telescope live grep" } )
-
--- Custom HJKL motion in insert mode
-vim.keymap.set("i", "<C-h>", "<Left>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-j>", "<Down>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-k>", "<Up>", { noremap = true, silent = true })
-vim.keymap.set("i", "<C-l>", "<Right>", { noremap = true, silent = true })
